57d9616c94782632950f742e759ff9474c7e6197
kralsito <franco_kral@hotmail.com>
2025-03-15 16:36:29 -0300

    feature: ajustes en la navbar

--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -3,15 +3,17 @@ import Navbar from "./components/navbar";
 import WhatsAppButton from "./components/whatsappbutton";
 
 const Layout = ({ children }) => {
-    return (
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">{children}</main>
-        <WhatsAppButton />
-        <Footer />
-      </div>
-    );
-  };
-  
-  export default Layout;
-  
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      {/* Aumentamos el padding para pantallas grandes */}
+      <main className="flex-grow pt-[calc(4rem+48px)] md:pt-[calc(5rem+64px)] lg:pt-[calc(6rem+72px)]">
+        {children}
+      </main>
+      <WhatsAppButton />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
