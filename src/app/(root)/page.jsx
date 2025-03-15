56e43907ecdeec446cc92a82ddcc94e0a122e799
kralsito <franco_kral@hotmail.com>
2025-03-14 21:19:48 -0300

    feature: section footer

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,11 +1,13 @@
 import ProductCarousel from "./components/productcarousel";
 import SectionACarousel from "./components/SectionACarousel";
-
+import SectionFooter from "./components/sectionFooter";
 export default function Home() {
     return (
         <>
             <SectionACarousel />
             <ProductCarousel />
+            <SectionFooter />
+            
         </>
     );
 }
