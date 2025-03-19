6f8ddd5f0e2c39b624ef6f4c3b5394038d9125bd
kralsito <franco_kral@hotmail.com>
2025-03-19 20:28:19 -0300

    feature: agrego modal para ver los precios y dar de alta formularios

---
+++ src/lib/api/form.js
@@ -0,0 +1,19 @@
+import 'server-only'
+import { apiRequest } from '../apiRequest'
+
+export async function getForms() {
+    return await apiRequest(`/form?page=0&size=20`, 'GET', null, 'application/json', false)
+  }
+
+  export async function createForm(formData) { 
+    const params = new URLSearchParams();
+    params.append('firstName', formData.firstName);
+    params.append('lastName', formData.lastName); 
+    params.append('phoneNumber', formData.phoneNumber);
+    
+    return await apiRequest(`/form?${params.toString()}`, 'POST', null, 'application/json', false);
+}
+
+export async function checkPhoneNumberExists(phoneNumber) {
+    return await apiRequest(`/form/exist?phoneNumber=${phoneNumber}`, 'GET', null, 'application/json', false);
+  }
